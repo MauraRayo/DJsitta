7a1732c20baf2832d7182640cb26b17799569e1f
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2026-05-06 11:09:52 +0200

    Home Terminado
    Diseño de los botons
    Linea span
    Añadir degradado en la img,

--- src/component/pages/home.jsx
+++ src/component/pages/home.jsx
@@ -6,10 +6,11 @@ export default function Home() {
     <section className="py-5 home">
       <div className="container">
         <div className="row align-items-center">
-          {/* Columna izquierda */}
-          <div className="col-md-6">
+          <div className="col-md-6  card-izq">
             <span className="title">DJ / Productora</span>
-            <h1 className="fw-bold mb-3 name">SITTA</h1>
+            <h1 className="mb-3 name">
+              <span className="lyric">SI</span>TTA
+            </h1>
 
             <p className="mb-4 description">
               DJ especializado en mezclas electrónicas, eventos privados y
@@ -17,24 +18,19 @@ export default function Home() {
               producciones personalizadas.
             </p>
 
-            <div className="d-flex gap-3">
-              <a className="btn btn-primary" href="#">
+            <div className="d-flex gap-4">
+              <a className="btn btn-listen" href="#">
                 Escuchar
               </a>
 
-              <a className="btn btn-outline-light" href="#">
+              <a className="btn btn-contact" href="#">
                 Contacto
               </a>
             </div>
           </div>
 
-          {/* Columna derecha */}
-          <div className="col-md-6 text-center mt-4 mt-md-0">
-            <img
-              src={aboutImg}
-              alt="DJ Sitta"
-              className="img-fluid rounded shadow"
-            />
+          <div className="col-md-6 mt-4 mt-md-0">
+            <img src={aboutImg} alt="DJ Sitta" className="img-fluid img-der" />
           </div>
         </div>
       </div>
